f01d7e5424397ed2bc0678733a7fbc6a77dc6a65
risxbh <82149801+risxbh@users.noreply.github.com>
2025-03-03 10:50:51 +0530

    Updates

--- src/Components/Core/Chat/ReusableComponents/SingleChat.jsx
+++ src/Components/Core/Chat/ReusableComponents/SingleChat.jsx
@@ -1,8 +1,8 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedChat } from "../../../../slices/chatSlice";
-import { Box, Typography, IconButton,FormControl,TextField } from "@mui/material";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Box, Typography, IconButton, FormControl, TextField } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import ProfileModal from "./ProfileModal";
 import { getSender, getSenderFull } from "../../../../config/chatlogics";
 import UpdateGroupChatModal from "./UpdateGroupChatModal";
@@ -10,28 +10,28 @@ import Loader from "../../../Common/Loader";
 import { chatEndpoints } from "../../../../services/apis";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import  "../styles.css";
+import "../styles.css";
 import ScrollableChat from "./ScrollableChat";
 import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../../../../animations/typing.json";
 import { setNotification } from "../../../../slices/chatSlice";
 const ENDPOINT = "https://nityambackend.onrender.com";
-var socket,selectedChatCompare; 
+var socket, selectedChatCompare;
 
 
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const dispatch = useDispatch();
-  const { selectedChat,notification } = useSelector((state) => state.chat);
+  const { selectedChat, notification } = useSelector((state) => state.chat);
   const { user } = useSelector((state) => state.profile);
-  const {token}=useSelector((state)=>state.auth);
-  const [messages,setMessages]=useState([]);
-  const [loading,setLoading]=useState(false);
-  const [newMessage,setNewMessage]=useState();
-  const [socketConnected,setSocketConnected]=useState(false);
-  const [typing,setTyping]=useState(false);
-  const [istyping,setIsTyping]=useState(false);
+  const { token } = useSelector((state) => state.auth);
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newMessage, setNewMessage] = useState();
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [istyping, setIsTyping] = useState(false);
 
 
   const defaultOptions = {
@@ -48,34 +48,34 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     dispatch(setSelectedChat(""));
   };
 
-  const fetchMessages=async()=>{
-    if(!selectedChat){
-        return;
+  const fetchMessages = async () => {
+    if (!selectedChat) {
+      return;
     }
     try {
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        };
-        setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      setLoading(true);
 
-        const {data}=await axios.get(`https://nityambackend.onrender.com/api/v1/message/${selectedChat._id}`,config);
-        setMessages(data.messages);
-        setLoading(false);
-        socket.emit("join chat",selectedChat._id);
+      const { data } = await axios.get(`https://nityambackend.onrender.com/api/v1/message/${selectedChat._id}`, config);
+      setMessages(data.messages);
+      setLoading(false);
+      socket.emit("join chat", selectedChat._id);
 
     } catch (error) {
-        console.log(error);
-        toast.error("Error in fetching the message");
+      console.log(error);
+      toast.error("Error in fetching the message");
     }
   }
 
 
-  
+
   const sendMessage = async (event) => {
     if (event.key === "Enter" && newMessage) {
-        socket.emit("stop typing",selectedChat._id)
+      socket.emit("stop typing", selectedChat._id)
       try {
         const config = {
           headers: {
@@ -100,64 +100,64 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       }
     }
   };
-  
+
   useEffect(() => {
     socket = io(ENDPOINT);
-    socket.emit("setup",user);
-    socket.on("connected",()=>setSocketConnected(true))
-    socket.on("typing",()=>setIsTyping(true));
-    socket.on("stop typing",()=>setIsTyping(false))
+    socket.emit("setup", user);
+    socket.on("connected", () => setSocketConnected(true))
+    socket.on("typing", () => setIsTyping(true));
+    socket.on("stop typing", () => setIsTyping(false))
   }, [])
 
 
   useEffect(() => {
     fetchMessages();
-    selectedChatCompare=selectedChat;
+    selectedChatCompare = selectedChat;
   }, [selectedChat])
 
 
-  console.log(notification,"------------>")
+  console.log(notification, "------------>")
   useEffect(() => {
-    socket.on("message recieved",(newMessageRecieved)=>{
-        if(
-            !selectedChatCompare || 
-            selectedChatCompare._id !== newMessageRecieved.chat._id
-        ){
-            if(!notification.includes(newMessageRecieved)){
-                dispatch(setNotification([newMessageRecieved,...notification]));
-                
-            }
+    socket.on("message recieved", (newMessageRecieved) => {
+      if (
+        !selectedChatCompare ||
+        selectedChatCompare._id !== newMessageRecieved.chat._id
+      ) {
+        if (!notification.includes(newMessageRecieved)) {
+          dispatch(setNotification([newMessageRecieved, ...notification]));
+
         }
-        else{
-            setMessages([...messages,newMessageRecieved]);
-        }
+      }
+      else {
+        setMessages([...messages, newMessageRecieved]);
+      }
     })
   })
- 
 
-  const typingHandler=(e)=>{
+
+  const typingHandler = (e) => {
     setNewMessage(e.target.value);
 
-    if(!socketConnected) return;
+    if (!socketConnected) return;
 
     if (!typing) {
-        setTyping(true);
-        socket.emit("typing", selectedChat._id);
+      setTyping(true);
+      socket.emit("typing", selectedChat._id);
+    }
+    let lastTypingTime = new Date().getTime();
+    var timerLength = 3000;
+    setTimeout(() => {
+      var timeNow = new Date().getTime();
+      var timeDiff = timeNow - lastTypingTime;
+      if (timeDiff >= timerLength && typing) {
+        socket.emit("stop typing", selectedChat._id);
+        setTyping(false);
       }
-      let lastTypingTime = new Date().getTime();
-      var timerLength = 3000;
-      setTimeout(() => {
-        var timeNow = new Date().getTime();
-        var timeDiff = timeNow - lastTypingTime;
-        if (timeDiff >= timerLength && typing) {
-          socket.emit("stop typing", selectedChat._id);
-          setTyping(false);
-        }
-      }, timerLength);
+    }, timerLength);
   }
 
 
-  
+
   return (
     <div>
       {selectedChat ? (
@@ -173,13 +173,45 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                 display={{ xs: "flex", md: "none" }}
                 onClick={handleBackButtonClick}
               >
-                <ArrowBackIcon />
+                <CloseIcon />
               </IconButton>
-              <Typography variant="h4" fontFamily="Work sans">
-                {!selectedChat.isGroupChat
-                  ? getSender(user, selectedChat.users)
-                  : selectedChat.chatName.toUpperCase()}
-              </Typography>
+              {selectedChat.isGroupChat ? (
+                <div class="flex relative items-center mr-1">
+                  {selectedChat.users.slice(0, 4).map((user, index) => (
+                    <img
+                      key={index}
+                      src={user.image}
+                      alt={user.firstName}
+                      class={`relative ${index === 0 ? '' : `ml-[-10px]`
+                        }`}
+                      style={{ width: '40px', height: '40px', borderRadius: '50%', zIndex: 10 - index }}
+                    />
+                  ))}
+                </div>
+              ) : (
+                <img
+                  src={
+                    selectedChat.latestMessage &&
+                    selectedChat.latestMessage.sender &&
+                    selectedChat.latestMessage.sender.image
+                  }
+                  alt={
+                    selectedChat.latestMessage &&
+                    selectedChat.latestMessage.sender &&
+                    selectedChat.latestMessage.sender.firstName
+                  }
+                  class="mr-2"
+                  style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+                />
+              )}
+              <div className="flex flex-col">
+                <h6 className="text-lg font-semibold text-gray-800 flex items-center">
+                  {!selectedChat.isGroupChat
+                    ? getSender(user, selectedChat.users)
+                    : selectedChat.chatName.toUpperCase()}
+                </h6>
+                <p class="text-sm text-gray-600 "> {selectedChat.isGroupChat ? `${selectedChat.users.length} Members` : null}</p>
+              </div>
             </Box>
             {!selectedChat.isGroupChat && (
               <ProfileModal user={getSenderFull(user, selectedChat.users)} />
@@ -192,38 +224,31 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
               />
             )}
           </Box>
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="flex-end"
-            p={3}
-            backgroundColor="#E8E8E8"
-            width="100%"
-            height="73vh"
-            borderRadius="10px"
-            overflowY="hidden"
+          <div
+            className="flex flex-col justify-end p-3 w-full "
+
           >
-            {loading ? (<Loader />):(<div  className="messages">
-                <ScrollableChat messages={messages}></ScrollableChat>
+            {loading ? (<Loader />) : (<div className="messages">
+              <ScrollableChat messages={messages}></ScrollableChat>
             </div>)}
             <FormControl onKeyDown={sendMessage} isRequired mt={3}>
-            {istyping ? (
+              {istyping ? (
                 <div>
                   <Lottie
                     options={defaultOptions}
                     // height={50}
                     width={70}
 
-                    style={{ marginBottom: 15,borderRadius:"10px", marginLeft: 0 }}
+                    style={{ marginBottom: 15, borderRadius: "10px", marginLeft: 0 }}
                   />
                 </div>
               ) : (
                 <></>
               )}
-            <TextField variant="filled" placeholder="Enter a message..." onChange={typingHandler} value={newMessage} />
+              <TextField variant="filled" placeholder="Enter a message..." onChange={typingHandler} value={newMessage} />
             </FormControl>
-          </Box>
-         
+          </div>
+
         </>
       ) : (
         <Box
